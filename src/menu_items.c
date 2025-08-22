f3149d4317aac7cc05faa669ab24beda86cfb34d
Fredrik Andréasson <medumee@gmail.com>
2025-08-22 11:10:30 +0200

    Added default menu transition for 3P4P options

--- src/menu_items.c
+++ src/menu_items.c
@@ -5101,6 +5101,11 @@ void draw_fade_in(s32 arg0, s32 arg1, s32 arg2) {
                     color->green, color->blue,
                     0xFF - (gCurrentTransitionTime[arg0] * 0xFF / gTransitionDuration[arg0]));
             }
+            else if ((arg0 == 4)) {
+                gDisplayListHead = draw_box_wide(gDisplayListHead, x - (w / 2), y - (h / 2), (w / 2) + x, (h / 2) + y,
+                                                 color->red, color->green, color->blue,
+                                                 0xFF - (gCurrentTransitionTime[arg0] * 0xFF / gTransitionDuration[arg0]));
+            }
             break;
     }
 
@@ -5546,6 +5551,9 @@ void func_8009D77C(s32 arg0, s32 arg1, s32 arg2) {
                 gDisplayListHead = draw_box_wide_pause_background(gDisplayListHead, var_t3 - temp_v1, var_t4 - temp_t8,
                                                                   rightEdge + someMath0, someMath1, temp_v0_2->red,
                                                                   temp_v0_2->green, temp_v0_2->blue, var_t2);
+            } else if ((arg0 == 4)) {
+                gDisplayListHead = draw_box_wide(gDisplayListHead, var_t3 - temp_v1, var_t4 - temp_t8, someMath0, someMath1,
+                                                 temp_v0_2->red, temp_v0_2->green, temp_v0_2->blue, var_t2);
             }
             break;
     }
@@ -5624,6 +5632,9 @@ void func_8009D998(s32 arg0) {
                 gDisplayListHead =
                     draw_box_wide_pause_background(gDisplayListHead, var_t0 - temp_v0, var_t1 - temp_v1,
                                                    rightEdge + someMath0, someMath1, 0, 0, 0, 0x000000FF);
+            } else if ((arg0 == 1) || (arg0 == 3)) {
+                gDisplayListHead = draw_box_wide(gDisplayListHead, var_t0 - temp_v0, var_t1 - temp_v1, someMath0, someMath1,
+                                                 0, 0, 0, 0x000000FF);
             }
             break;
     }
